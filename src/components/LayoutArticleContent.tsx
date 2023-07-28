53b5427c1c3b8bc081c95b8ea41e1fc77d8b97ee
a3318375 <184608371@qq.com>
2023-07-29 02:37:06 +0800

    123

--- src/components/LayoutArticleContent.tsx
+++ src/components/LayoutArticleContent.tsx
@@ -54,8 +54,30 @@ export default function LayoutArticleContent({children}: { children: React.React
             setTick(true)
         }
     }
+    const toc = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            // Add 'active' class if observation target is inside viewport
+            if (entry.isIntersecting) {
+                const tocLiList = document.querySelectorAll(".toc-list li");
+                Array.from(tocLiList,v => {
+                    const aLink = v.querySelector('.toc-link');
+                    if(aLink &&  aLink.getAttribute('href')?.replace('#','') === entry.target.id){
+                        v.classList.add("is-active-link")
+                        aLink.classList.add("is-active-link")
+                    }else{
+                        v.classList.remove("is-active-link")
+                        aLink?.classList.remove("is-active-link")
+                    }
+                });
+            }
+        })
+    })
+
     useEffect(() => {
         document.getElementById('pageContent')?.addEventListener('scroll', onScroll, false);
+        const tocList = document.querySelectorAll(".entry-content h1, h2, h3, h4, h5, h6");
+        // 监听滚动
+        Array.from(tocList).map(item => toc.observe(item));
     }, []);
     return (
         <div className="w-full h-screen grid overflow-hidden">
