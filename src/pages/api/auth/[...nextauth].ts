cf58b91cfea6d61c851c7b7b599db896963a7f44
a3318375 <184608371@qq.com>
2023-08-17 10:37:28 +0800

    123

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,8 +5,8 @@ export const authOptions = {
     // Configure one or more authentication providers
     providers: [
         GithubProvider({
-            clientId: '338ac6015e87671f569c',
-            clientSecret: '39b66e8efbc44bf02711ccf3cc7fc4dc7c744e01',
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
         }),
         // ...add more providers here
     ],
