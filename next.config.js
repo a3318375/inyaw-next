3e5c923d2323668f4215d185f902478e4cb5f1af
a3318375 <184608371@qq.com>
2024-10-23 21:04:27 +0800

    部署

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
     output: 'standalone',
-    assetPrefix: isProd ? 'https://media.inyaw.com/inyaw-nextjs' : undefined,
 }
 module.exports = nextConfig
